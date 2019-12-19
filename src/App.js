e29316e566a4e1138792845115e43357a87e3813
Anna Loukianova <38470773+annaloukianova@users.noreply.github.com>
2019-12-19 13:01:49 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <div>A test of a commit</div>
           <Signup createAccount={() => {}} />
         </header>
       </div>
